53b6bb5997524947bdab4d8cd67bae63670ab1c1
Nicksaihs <nickchang@saihs.edu.tw>
2025-02-18 15:55:31 +0800

    first commit

---
+++ firstproject/Form1.cs
@@ -0,0 +1,20 @@
+namespace firstproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "ÂW§Ú·F¹À";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
